1cd3415a81e6ead631b331ae53503f799d00e532
za <zali110@tamu.edu>
2021-03-06 10:03:15 -0800

    passed paramters to customerOptionMenu

--- project_2_gui/src/view/customerOptionMenu.java
+++ project_2_gui/src/view/customerOptionMenu.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class customerOptionMenu extends JFrame {
 
@@ -17,6 +18,12 @@ public class customerOptionMenu extends JFrame {
 	private final JButton btnViewLastMeal = new JButton("View Last Meal");
 	private final JButton btnPaymentInformation = new JButton("Payment Information");
 	private final JButton btnRewards = new JButton("Reward Tier");
+	protected static String first;
+	protected static String last;
+	protected static String user;
+	protected static String pass;
+	private final JLabel lblUser = new JLabel("User:");
+	private final JLabel labelUserGivenName = new JLabel("");
 
 	/**
 	 * Launch the application.
@@ -25,7 +32,7 @@ public class customerOptionMenu extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					customerOptionMenu frame = new customerOptionMenu();
+					customerOptionMenu frame = new customerOptionMenu(first, last, user, pass);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -36,8 +43,16 @@ public class customerOptionMenu extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @param password 
+	 * @param username 
+	 * @param last_name 
+	 * @param first_name 
 	 */
-	public customerOptionMenu() {
+	public customerOptionMenu(String first_name, String last_name, String username, String password) {
+		first = first_name;
+		last = last_name;
+		user = username;
+		pass = password;
 		initGUI();
 	}
 	private void initGUI() {
@@ -47,6 +62,7 @@ public class customerOptionMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		labelUserGivenName.setText(user);
 		btnViewMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(arg0.getSource() == btnViewMenu) {
@@ -54,7 +70,7 @@ public class customerOptionMenu extends JFrame {
 				}
 			}
 		});
-		btnViewMenu.setBounds(88, 12, 250, 48);
+		btnViewMenu.setBounds(88, 30, 250, 48);
 		
 		contentPane.add(btnViewMenu);
 		btnViewLastMeal.addActionListener(new ActionListener() {
@@ -64,7 +80,7 @@ public class customerOptionMenu extends JFrame {
 				}
 			}
 		});
-		btnViewLastMeal.setBounds(88, 72, 250, 48);
+		btnViewLastMeal.setBounds(88, 90, 250, 48);
 		
 		contentPane.add(btnViewLastMeal);
 		btnPaymentInformation.addActionListener(new ActionListener() {
@@ -74,7 +90,7 @@ public class customerOptionMenu extends JFrame {
 				}
 			}
 		});
-		btnPaymentInformation.setBounds(88, 132, 250, 48);
+		btnPaymentInformation.setBounds(88, 150, 250, 48);
 		
 		contentPane.add(btnPaymentInformation);
 		btnRewards.addActionListener(new ActionListener() {
@@ -84,9 +100,15 @@ public class customerOptionMenu extends JFrame {
 				}
 			}
 		});
-		btnRewards.setBounds(88, 194, 250, 48);
+		btnRewards.setBounds(88, 210, 250, 48);
 		
 		contentPane.add(btnRewards);
+		lblUser.setBounds(12, 3, 70, 15);
+		
+		contentPane.add(lblUser);
+		labelUserGivenName.setBounds(71, 3, 70, 15);
+		
+		contentPane.add(labelUserGivenName);
 	}
 
 }

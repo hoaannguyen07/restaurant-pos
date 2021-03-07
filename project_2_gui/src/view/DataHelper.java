304c83d24e15a2c7248a708bca6405807afef277
sms0325 <sms0325@tamu.edu>
2021-03-06 23:46:20 -0600

    Revert "reset customer & manager menus to use in house api & update data helper get menu to not create api connection again"
    This reverts commit 700c5798bec4ba4444a0197916728fe290186ceb.

--- project_2_gui/src/view/DataHelper.java
+++ project_2_gui/src/view/DataHelper.java
@@ -322,14 +322,15 @@ public class DataHelper {
 		return true;
 	}
 	
-	public Vector<Vector<String>> get_menu_data()
+	Vector<Vector<String>> get_menu_data()
 	{
 		Vector<Vector<String>> menu_list = new Vector<Vector<String>>();
+		DataHelper api_connection = new DataHelper();
 
 		try
 		{
 			
-			Statement stmt = this.conn.createStatement(); // statement object
+			Statement stmt = api_connection.conn.createStatement(); // statement object
 			// create the actual statement to populate the statement object
 			String sql_stmt = "SELECT * from menu";
 			
@@ -353,7 +354,6 @@ public class DataHelper {
 		{
 			System.out.println("Error adding to manager Datatable.");
 		}
-		System.out.println(menu_list);
 		return menu_list;
 	}
 	

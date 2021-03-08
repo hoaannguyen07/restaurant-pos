f0d841f1d9e5f662f396e51c777deb352fd91e98
reproach1 <reproach1@tamu.edu>
2021-03-07 18:06:07 -0600

    Revert "Merge branch 'master' of https://github.tamu.edu/zali110/CSCE315_project_2_group_9"
    This reverts commit 59505fdd491d1cef29762752fcbc9ab705a982e2, reversing
    changes made to 934fb054028f5d6b808365b9972426b4b24970dc.

---
+++ project_2_gui/src/view/ViewModel.java
@@ -0,0 +1,54 @@
+package src.view;
+
+import java.awt.Dimension;
+ 
+import javax.swing.BorderFactory;
+//import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+//import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import javax.swing.table.DefaultTableModel;
+ 
+//@author ashraf
+
+public class ViewModel {
+ 
+    public ViewModel() {
+        // Create views swing UI components 
+        JTable table = new JTable();
+ 
+        // Create table model
+
+        public static final Object[] TABLE_HEADER = { 
+        		"Item Name", "Item Type", "Price"
+        };
+
+        table.setModel(DataHelper.get_menu_data, TABLE_HEADER);
+ 
+        // Set the view layout
+        JPanel ctrlPane = new JPanel();
+ 
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        tableScrollPane.setPreferredSize(new Dimension(450, 250));
+        tableScrollPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Market Movers",
+                TitledBorder.CENTER, TitledBorder.TOP));
+ 
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, ctrlPane, tableScrollPane);
+        splitPane.setDividerLocation(35);
+        splitPane.setEnabled(false);
+ 
+        // Display it all in a scrolling window and make the window appear
+        JFrame frame = new JFrame("Swing MVC Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(splitPane);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+ 
+}

8d6c9664600b5ea4888c47662558bb227d7aeba5
hoaannguyen07 <hoaannguyen07@gmail.com>
2021-03-10 23:04:12 +0700

    Create Item_Info.java
    used in Cart class to map ingredients and menu

---
+++ project_2_gui/src/view/Item_Info.java
@@ -0,0 +1,53 @@
+package view;
+
+/**
+ * 
+ * @author hoaan
+ * used to keep name and price of item
+ */
+public class Item_Info {
+	private String name;
+	private String price;
+	
+	Item_Info()
+	{
+		this.name = "";
+		this.price = "";
+	}
+	
+	Item_Info(String item_name, String item_price)
+	{
+		this.name = item_name;
+		this.price = item_price;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public String getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+}
